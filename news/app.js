4c7cf0ddee53ba1f2c7f22eb7bbac2c53c77bab8
Ankit Chaurasia <ankit.chaurasia@ggktech.com>
2018-06-01 12:44:42 +0530

    News PWA, service worker and cache storage

---
+++ news/app.js
@@ -0,0 +1,50 @@
+const apiKey = 'c9ca698dd5b14410a9ff27c4277fe550';
+const main = document.querySelector('main');
+const sourceSelector = document.querySelector("#sourceSelector");
+const defaultSource = 'the-washington-post';
+
+window.addEventListener('load',  e => {
+    updateNews();
+    updateSourcs();
+    sourceSelector.value = defaultSource;
+
+    sourceSelector.addEventListener('change', e => {
+        updateNews(e.target.value);
+    });
+
+    if('serviceWorker' in navigator) {
+        try {
+            navigator.serviceWorker.register('serviceWorker.js');
+            console.log('Service worker registered');
+        } catch (error) {
+            console.log('Service worker failed');
+        }
+    }
+});
+
+async function updateSourcs() {
+    const res = await fetch(`https://newsapi.org/v1/sources`);
+    const json =  await res.json();
+    sourceSelector.innerHTML = json.sources.map(src => `<option value='${src.id}'>${src.name}</option>`).join('\n');
+    sourceSelector.value = defaultSource;
+}
+
+async function updateNews(source = defaultSource) {
+    const res = await fetch(`https://newsapi.org/v1/articles?source=${source}&apiKey=${apiKey}`);
+    const json = await res.json();
+    main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article) {
+    return `
+        <div class='article'>
+            <a href='${article.url}'>
+                <h2>${article.title}</h2>
+                <img src='${article.urlToImage}'/>
+                <p>${article.description}</p>
+            </a>
+        </div>
+    `;
+}
+
+  
---
+++ news/serviceWorker.js
@@ -0,0 +1,50 @@
+/**
+ * Cache all the assets of application to serve them from cache and get really fast start time for the
+ * application so we can display something when user is offiline
+ */ 
+const staticAssets = [
+    './',
+    './styles.css',
+    './app.js',
+    './fallback.json',
+    './images/fallback.png'
+];
+
+/**
+ * install events triggers when we install app
+ */
+self.addEventListener('install',  async event => {
+    const cache = await caches.open('news-static');
+    cache.addAll(staticAssets);
+});
+
+/**
+ * Fetch event triggers when service worker intercept any request going from your app
+ */
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    const url = new URL(req.url);
+    if (url.origin === location.origin) { // Fetching from own site
+        event.respondWith(cacheFirst(req));
+    } else {
+        event.respondWith(networkFirst(req))
+    }
+    
+});
+
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+    const cache = await caches.open('news-dynamic');
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    } catch(error) {
+        const cachedResponse = await cache.match(req);
+        return cachedResponse || caches.match('./fallback.json');
+    }
+}
